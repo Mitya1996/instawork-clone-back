c0e7cb04c526df6516b7550cb4c966f2dd7f2713
mits <dmitrybaycharov@gmail.com>
2021-11-06 10:33:17 -0500

    get all for all 3 collections functional locally

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from gfs_connection import db
+
 from typing import Optional
 
 from fastapi import FastAPI
@@ -13,3 +15,27 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+# gigs
+
+
+@app.get("/gigs")
+def read_root():
+    gigs_ref = db.collection('gigs')
+    return {doc.id: doc.to_dict() for doc in gigs_ref.stream()}
+
+# companies
+
+
+@app.get("/companies")
+def read_root():
+    companies_ref = db.collection('companies')
+    return {doc.id: doc.to_dict() for doc in companies_ref.stream()}
+
+# pros
+
+
+@app.get("/pros")
+def read_root():
+    pros_ref = db.collection('pros')
+    return {doc.id: doc.to_dict() for doc in pros_ref.stream()}

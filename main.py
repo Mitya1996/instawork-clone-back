a1a4dc2482de41ea7cf109f07c1ea9a585608eb4
mits <dmitrybaycharov@gmail.com>
2021-11-17 11:53:40 +0100

    read one gig at a time

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# sample queries
+
 
 @app.get("/")
 def read_root():
@@ -18,17 +20,33 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 # gigs
 
+    # READ ALL
+
 
 @app.get("/gigs")
-def read_root():
+def read_gigs():
     gigs_ref = db.collection('gigs')
     return {doc.id: doc.to_dict() for doc in gigs_ref.stream()}
+
+    # READ ONE
+
+
+@app.get("/gigs/{gig_id}")
+def read_gig(gig_id: str):
+    doc_ref = db.collection('gigs').document(gig_id)
+
+    doc = doc_ref.get()
+    if doc.exists:
+        return {doc.id: doc.to_dict()}
+    else:
+        return {doc.id: 'No such document!'}
+
 
 # companies
 
 
 @app.get("/companies")
-def read_root():
+def read_companies():
     companies_ref = db.collection('companies')
     return {doc.id: doc.to_dict() for doc in companies_ref.stream()}
 
@@ -36,6 +54,6 @@ def read_root():
 
 
 @app.get("/pros")
-def read_root():
+def read_pros():
     pros_ref = db.collection('pros')
     return {doc.id: doc.to_dict() for doc in pros_ref.stream()}

d47a46619bfc694dd60bfefb110887f049c0800b
mits <dmitrybaycharov@gmail.com>
2021-11-17 13:51:38 +0100

    added metadata

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ from typing import Optional
 
 from fastapi import FastAPI
 
-app = FastAPI()
+description = """
+Instawork Clone is a clone of the Instawork app that allows anyone quickly and easily find high paying gigs in their area! 🚀
+
+## Gigs
+
+You can **create, read, update, and delete gigs**.
+
+## Companies
+
+You can **create, read, update, and delete companies**.
+
+## Pros
+
+You can **create, read, update, and delete pros**.
+
+"""
+
+app = FastAPI(title="Instawork Clone Backend", description=description)
 
 # sample queries
 
@@ -24,7 +41,7 @@ def read_root():
     # READ ALL
 
 
-@app.get("/gigs")
+@app.get("/gigs", tags=["gigs"])
 def read_gigs():
     gigs_ref = db.collection('gigs')
     return {doc.id: doc.to_dict() for doc in gigs_ref.stream()}
@@ -32,7 +49,7 @@ def read_gigs():
     # READ ONE
 
 
-@app.get("/gigs/{gig_id}")
+@app.get("/gigs/{gig_id}", tags=["gigs"])
 def read_gig(gig_id: str):
     doc_ref = db.collection('gigs').document(gig_id)
 
@@ -45,7 +62,7 @@ def read_gig(gig_id: str):
     # CREATE ONE
 
 
-@app.post("/gigs/")
+@app.post("/gigs/", tags=["gigs"])
 async def create_gig(gig: Gig):
     db.collection('gigs').document().set(gig.dict())
 
@@ -55,7 +72,7 @@ async def create_gig(gig: Gig):
 # companies
 
     # READ ALL
-@app.get("/companies")
+@app.get("/companies", tags=["companies"])
 def read_companies():
     companies_ref = db.collection('companies')
     return {doc.id: doc.to_dict() for doc in companies_ref.stream()}
@@ -63,7 +80,7 @@ def read_companies():
     # READ ONE
 
 
-@app.get("/companies/{company_id}")
+@app.get("/companies/{company_id}", tags=["companies"])
 def read_company(company_id: str):
     doc_ref = db.collection('companies').document(company_id)
 
@@ -76,7 +93,7 @@ def read_company(company_id: str):
     # CREATE ONE
 
 
-@app.post("/companies/")
+@app.post("/companies/", tags=["companies"])
 async def create_company(company: Company):
     db.collection('companies').document().set(company.dict())
 
@@ -86,7 +103,7 @@ async def create_company(company: Company):
 # pros
 
     # READ ALL
-@app.get("/pros")
+@app.get("/pros", tags=["pros"])
 def read_pros():
     pros_ref = db.collection('pros')
     return {doc.id: doc.to_dict() for doc in pros_ref.stream()}
@@ -94,7 +111,7 @@ def read_pros():
     # READ ONE
 
 
-@app.get("/pros/{pro_id}")
+@app.get("/pros/{pro_id}", tags=["pros"])
 def read_company(pro_id: str):
     doc_ref = db.collection('pros').document(pro_id)
 
@@ -107,7 +124,7 @@ def read_company(pro_id: str):
     # CREATE ONE
 
 
-@app.post("/pros/")
+@app.post("/pros/", tags=["pros"])
 async def create_pro(pro: Pro):
     db.collection('pros').document().set(pro.dict())
 

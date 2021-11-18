91b80ce2f23cb12ff6cc571a98907fa84a1b5584
mits <dmitrybaycharov@gmail.com>
2021-11-18 19:18:31 +0100

    test

--- gfs_connection.py
+++ gfs_connection.py
@@ -6,23 +6,23 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-def cred():
-    GOOGLE_APPLICATION_CREDENTIALS_FILENAME = 'key.json'
-    # set GOOGLE_APPLICATION_CREDENTIALS_TEXT in Cloud Run and cloudbuild.yaml to service acct key (from secret mgr)
-    GOOGLE_APPLICATION_CREDENTIALS_TEXT = os.environ.get(
-        'GOOGLE_APPLICATION_CREDENTIALS_TEXT')
+# def cred():
+#     GOOGLE_APPLICATION_CREDENTIALS_FILENAME = 'key.json'
+#     # set GOOGLE_APPLICATION_CREDENTIALS_TEXT in Cloud Run and cloudbuild.yaml to service acct key (from secret mgr)
+#     GOOGLE_APPLICATION_CREDENTIALS_TEXT = os.environ.get(
+#         'GOOGLE_APPLICATION_CREDENTIALS_TEXT')
 
-    # if in cloud run environment
-    if GOOGLE_APPLICATION_CREDENTIALS_TEXT:
-        file = open(GOOGLE_APPLICATION_CREDENTIALS_FILENAME, 'w')
-        file.write(GOOGLE_APPLICATION_CREDENTIALS_TEXT)
-        file.close()
-        return credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS_FILENAME)
-    # if doing local development/testing
-    return credentials.ApplicationDefault()
+#     # if in cloud run environment
+#     if GOOGLE_APPLICATION_CREDENTIALS_TEXT:
+#         file = open(GOOGLE_APPLICATION_CREDENTIALS_FILENAME, 'w')
+#         file.write(GOOGLE_APPLICATION_CREDENTIALS_TEXT)
+#         file.close()
+#         return credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS_FILENAME)
+#     # if doing local development/testing
+#     return credentials.ApplicationDefault()
 
 
-firebase_admin.initialize_app(cred(), {
+firebase_admin.initialize_app(credentials.Certificate('/gcp/key.json'), {
     'projectId': 'instawork-clone',
 })
 

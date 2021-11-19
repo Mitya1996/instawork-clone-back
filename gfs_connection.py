07f542fa66b02ff16cc3d94c8b61c29a6a074be8
mits <dmitrybaycharov@gmail.com>
2021-11-19 09:29:23 +0100

    firestore connection refactoring

--- gfs_connection.py
+++ gfs_connection.py
@@ -5,29 +5,6 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-# def cred():
-#     GOOGLE_APPLICATION_CREDENTIALS_FILENAME = 'key.json'
-#     # set GOOGLE_APPLICATION_CREDENTIALS_TEXT in Cloud Run and cloudbuild.yaml to service acct key (from secret mgr)
-#     GOOGLE_APPLICATION_CREDENTIALS_TEXT = os.environ.get(
-#         'GOOGLE_APPLICATION_CREDENTIALS_TEXT')
-
-#     # if in cloud run environment
-#     if GOOGLE_APPLICATION_CREDENTIALS_TEXT:
-#         file = open(GOOGLE_APPLICATION_CREDENTIALS_FILENAME, 'w')
-#         file.write(GOOGLE_APPLICATION_CREDENTIALS_TEXT)
-#         file.close()
-#         return credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS_FILENAME)
-#     # if doing local development/testing
-#     return credentials.ApplicationDefault()
-
-
-# firebase_admin.initialize_app(cred(), {
-#     'projectId': 'instawork-clone',
-# })
-
-# db = firestore.client()
-
-
 # if there is a /gcp path then
 import os
 gcp = os.path.isfile('/gcp')
